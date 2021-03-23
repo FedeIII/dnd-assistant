0518a2f450837f97d99045201d2b45f2938a883c
FedeIII <federico.perez@coverwallet.com>
2021-03-23 23:26:12 +0100

    scaffolding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import screens from './screens/constants';
+import RollDice from './screens/rollDice';
+import './App.scss';
 
 function App() {
+  const [screen, setScreen] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        DnD Assistant
       </header>
+      <div className="body">
+        <div className="sidebar">
+          <button onClick={() => setScreen(screens.ROLL)}>Roll Dice</button>
+          <button>Button 2</button>
+          <button>Button 3</button>
+        </div>
+        <div className="content">
+          {(!screen) && <div>Welcome</div>}
+          {(screen == screens.ROLL) && <RollDice />}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/screens/rollDice.jsx
@@ -0,0 +1,5 @@
+function RollDice() {
+  return <div>Roll Dice</div>
+}
+
+export default RollDice;
