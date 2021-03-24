1101fbcf254f29a5e46ba2d5168bf66b958ddcfe
Federico Perez <federico.perez@coverwallet.com>
2021-03-24 01:51:36 +0100

    added command line

--- src/screens/rollDice.jsx
+++ src/screens/rollDice.jsx
@@ -1,5 +1,34 @@
+import { useState } from 'react';
+
 function RollDice() {
-  return <div>Roll Dice</div>
+  const [command, setCommand] = useState('');
+  const [history, setHistory] = useState([]);
+
+  const onCommandChange = event => {
+    setCommand(event.target.value);
+  }
+
+  const onCommandSubmit = event => {
+    event.preventDefault();
+
+    if (command) {
+      setCommand('');
+      setHistory(history.concat(command));
+    }
+  };
+
+  return (
+      <div className="terminal">
+        <div className="history">
+          {history.map(line => <p key={line} className="history-line">{line}</p>)}
+          <div id="history-anchor" />
+        </div>
+        <form onSubmit={onCommandSubmit}>
+          <input className="command" value={command} onChange={onCommandChange}/>
+          <input type="submit" className="submit-command"/>
+        </form>
+      </div>
+  );
 }
 
 export default RollDice;
